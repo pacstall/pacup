631b35a60852813c20045792f7354868f6a9ceeb
V. <150687949+vigress8@users.noreply.github.com>
2024-10-07 12:43:27 +0400

    feat: add --test flag, set LC_ALL=C (#27)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 {
   pkgs ?
-    import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11")
+    import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05")
     {
       config = {};
       overlays = [];
